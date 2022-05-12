f8f5ad4afecec5168a26d06e4c40c9bdaaad1a9f
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-05-13 00:08:11 +0200

    Separate function for logging into the website

---
+++ page_login.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import keyboard
+import os
+import time
+
+DRIVER_PATH = r'C:\Program Files\chromedriver.exe'
+
+USERNAME = os.environ['USERNAME']
+LOGIN = os.environ['LOGIN']
+PASSW = os.environ['PASSW']
+
+option = Options()
+option.add_argument("--disable-infobars")
+option.add_argument("--disable-extensions")
+option.add_experimental_option("prefs", {
+    "profile.default_content_setting_values.notifications": 1
+})
+
+def login():
+    driver = webdriver.Chrome(DRIVER_PATH, options=option)
+    driver.maximize_window()
+
+    url = f"https://www.filmweb.pl/user/{USERNAME}/friends"
+
+    driver.get(url)
+
+    agree_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.ID, 'didomi-notice-agree-button')))
+    agree_button.click()
+
+    time.sleep(5)
+
+    try:
+        skip_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, 'ws__skipButton')))
+        skip_button.click()
+    except TimeoutException or NoSuchElementException:
+        pass
+
+    time.sleep(10)
+
+    facebook_login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.CLASS_NAME, 'facebookLoginButton__text')))
+    facebook_login.click()
+
+    time.sleep(5)
+
+    allow_cookies = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]')))
+    allow_cookies.click()
+
+    time.sleep(5)
+
+    login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.XPATH, "//input[@type='text']")))
+    login.click()
+    keyboard.write(LOGIN)
+    passw = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.XPATH, "//input[@type='password']")))
+    passw.click()
+    keyboard.write(PASSW)
+    keyboard.press_and_release('enter')
+    return driver, url
