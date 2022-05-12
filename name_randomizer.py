9f202dded4c674e8014b43a1e8fc6161d2c5d141
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-05-13 00:49:15 +0200

    Simple name randomizer

---
+++ name_randomizer.py
@@ -0,0 +1,16 @@
+import names
+import random
+import pandas as pd
+
+df = pd.read_excel('friendslist.xlsx', header=0, index_col=0)
+
+friends_num = df['Name'].shape[0]
+
+for i in range(friends_num):
+    name, surname = names.get_full_name().split()
+    username = name.lower() + surname[:3].lower() + str(random.randint(1,9))
+    df['Name'].at[i+1] = name
+    df['Surname'].at[i+1] = surname
+    df['Username'].at[i+1] = username
+
+df.to_excel('friendslist_randomized.xlsx')
