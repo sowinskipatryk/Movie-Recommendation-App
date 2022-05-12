0c7da192846ddf4ffde9bb2b35efc6bae5875a49
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-05-13 00:25:39 +0200

    Scraping friends list with login

--- friendlist_collector.py
+++ friendlist_collector.py
@@ -1,72 +1,12 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import time
-import keyboard
 import pandas as pd
-import os
+from page_login import login
 
-DRIVER_PATH = r'C:\Program Files\chromedriver.exe'
-
-USERNAME = os.environ['USERNAME']
-LOGIN = os.environ['LOGIN']
-PASSW = os.environ['PASSW']
-
-option = Options()
-option.add_argument("--disable-infobars")
-option.add_argument("--disable-extensions")
-option.add_experimental_option("prefs", {
-    "profile.default_content_setting_values.notifications": 1
-})
-
-driver = webdriver.Chrome(DRIVER_PATH, options=option)
-driver.maximize_window()
-
-url = f"https://www.filmweb.pl/user/{USERNAME}/friends"
-
-driver.get(url)
-
-agree_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-    (By.ID, 'didomi-notice-agree-button')))
-agree_button.click()
-
-time.sleep(5)
-
-skip_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-    (By.CLASS_NAME, 'ws__skipButton')))
-skip_button.click()
-
-time.sleep(10)
-
-facebook_login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-    (By.CLASS_NAME, 'facebookLoginButton__text')))
-facebook_login.click()
-
-time.sleep(5)
-
-allow_cookies = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-    (By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]')))
-allow_cookies.click()
-
-time.sleep(5)
-
-login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-    (By.XPATH, "//input[@type='text']")))
-login.click()
-keyboard.write(LOGIN)
-passw = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-    (By.XPATH, "//input[@type='password']")))
-passw.click()
-keyboard.write(PASSW)
-keyboard.press_and_release('enter')
-
+driver, url = login()
 time.sleep(5)
 
 driver.get(url)
-
 time.sleep(10)
 
 step = 1000
@@ -74,7 +14,7 @@ i = 1
 flag = True
 while flag:
     driver.execute_script(f"window.scrollTo(0, {i*step});")
-    time.sleep(1.5)
+    time.sleep(4)
     curr_page_height = driver.execute_script(
         "return document.body.scrollHeight")
 
@@ -88,7 +28,8 @@ page_html = driver.page_source
 soup = BeautifulSoup(page_html, 'html.parser')
 
 friendslist = []
-friends = soup.find_all('div', {'class': 'user__body'})
+friend_section = soup.find('section', {'class': 'section__userFriends'})
+friends = friend_section.find_all('div', {'class': 'user__body'})
 for friend in friends:
     name = friend.find('span', {'class':'user__firstName'})
     if name:
@@ -101,6 +42,6 @@ for friend in friends:
 
 df = pd.DataFrame(friendslist, columns=['Name', 'Surname', 'Username'],
                   index=range(1, len(friendslist)+1))
-df.to_excel('friendslist.xlsx')
+df.to_excel('friendslist2.xlsx')
 
 driver.close()
